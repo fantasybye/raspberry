4e1359f49c06d5b2fe500e69af6e327a2f01642f
DongCanRola <dongcanthis@outlook.com>
2017-04-26 17:15:43 +0800

    Update sqlope.js

--- sqlope.js
+++ sqlope.js
@@ -1,13 +1,10 @@
 function sqlope(){
   var sqlite3=require('sqlite3').verbose();
-  //var db=new sqlite3.Database('devsensor');
   this.savedev=function(devid,devname,description,registertime){
     var db=new sqlite3.Database('devsensor');
     var stmt=db.prepare("INSERT INTO devs VALUES(?,?,?,?)");
     stmt.run(devid,devname,description,registertime);
     stmt.finalize();
-    //db.run("INSERT INTO devs values("+devid+","+devname+","+description+")");
-    //db.finalize();
     db.close();
   };
   this.savesensor=function(sensorid,devid,sensorname,description,measure,measurecharacter,registertime){
@@ -15,23 +12,7 @@ function sqlope(){
     var stmt=db.prepare("INSERT INTO sensors VALUES(?,?,?,?,?,?,?)");
     stmt.run(sensorid,devid,sensorname,description,measure,measurecharacter,registertime);
     stmt.finalize();
-    //db.run("INSERT INTO sensors values("+sensorid+","+devid+","+sensorname+","+description+","+measure+","+measurecharacter+")");
-    //db.finalize();
     db.close();
-  };
-  this.selectdevid=function(devname,registertime){
-    var db=new sqlite3.Database('devsensor');
-    var stmt=db.prepare("SELECT devid FROM devs WHERE devname=? and registertime=?");
-    var id=stmt.run(devname,registertime);
-    stmt.finalize();
-    return id;
-  };
-  this.selectsensorid=function(devid,sensorname,registertime){
-    var db=new sqlite3.Database('devsensor');
-    var stmt=db.prepare("SELECT sensorid FROM sensors WHERE devid=? and sensorname=? and registertime=?");
-    var id=stmt.run(devid,sensorname,registertime);
-    stmt.finalize();
-    return id;
   };
 }
 module.exports=sqlope;
